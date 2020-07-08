89508085f6d25467b7857cbc580dfde82d147416
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-08 17:43:14 -0600

    showing errors and loading text

--- src/components/Products.js
+++ src/components/Products.js
@@ -19,11 +19,18 @@ const Products = () => {
 
     //get the state
     const products = useSelector(state => state.products.products);
+    const error = useSelector(state => state.products.error);
+    const loading = useSelector(state => state.products.loading);
 
     return ( 
         <Fragment>
             <h2 className="text-center my-5">Product Listing</h2>
 
+            { error ? <p className="font-weight-bold alert alert-danger text-center mt-4">
+                There was an error</p> : null }
+
+            { loading ? <p className="text-center">Loading...</p> : null }
+            
             <table className="table table-striped">
                 <thead className="bg-primary table-dark">
                     <tr>
