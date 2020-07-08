379366dd003d482110c39d1b9a46cb04a9518089
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-08 17:33:56 -0600

    showing the projects in the interface

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect } from 'react';
+import Product from './Product';
 
 //Redux
 import { useSelector, useDispatch } from 'react-redux';
@@ -16,6 +17,9 @@ const Products = () => {
 
     }, []);
 
+    //get the state
+    const products = useSelector(state => state.products.products);
+
     return ( 
         <Fragment>
             <h2 className="text-center my-5">Product Listing</h2>
@@ -29,7 +33,14 @@ const Products = () => {
                     </tr>
                 </thead>
                 <tbody>
-
+                    { products.length === 0 ? 'There are not Products' : (
+                        products.map(product => (
+                            <Product 
+                                key={product.id}
+                                product={product}
+                            />
+                        ))
+                    )}
                 </tbody>
             </table>
         </Fragment>
