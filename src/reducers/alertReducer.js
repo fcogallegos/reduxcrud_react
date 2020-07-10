8c0e1d538114997e0d489474d7585ab08db3f47d
Francisco Gallegos <fgallegosgo@gmail.com>
2020-07-09 18:24:39 -0600

    importing the alerts

--- src/reducers/alertReducer.js
+++ src/reducers/alertReducer.js
@@ -11,6 +11,11 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case SHOW_ALERT:
+            return {
+                ...state,
+                alert: action.payload
+            }
         default:
             return state;
     }
